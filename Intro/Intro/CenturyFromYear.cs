bdfb80f9aa3cc035891becf48b3fc3653fc458ae
Gayane <gayaneh001@gmail.com>
2019-05-02 00:24:46 +0400

    Initial commit

--- Intro/Intro/CenturyFromYear.cs
+++ Intro/Intro/CenturyFromYear.cs
@@ -6,6 +6,5 @@
         {
             return year % 100 == 0 ? year / 100 : year / 100 + 1;
         }
-
     }
 }
---
+++ Intro/Intro/CheckPalindrome.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Intro
+{
+    class CheckPalindrome
+    {
+        bool checkPalindrome(string inputString)
+        {
+            return inputString == new string(inputString.ToCharArray().Reverse().ToArray());
+        }
+    }
+}
