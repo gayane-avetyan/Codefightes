baf60e33f5d61a493ab5ef144f927af5d4bbc72b
Gayane <gayaneh001@gmail.com>
2019-05-01 23:47:03 +0400

    Initial commit

---
+++ Intro/Intro/CenturyFromYear.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro
+{
+    class CenturyFromYear
+    {
+        int centuryFromYear(int year)
+        {
+            return year % 100 == 0 ? year / 100 : year / 100 + 1;
+        }
+
+    }
+}
