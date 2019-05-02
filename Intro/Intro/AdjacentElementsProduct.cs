039b95ee143fcb327007d6577a7dd61266f7a9a0
Gayane <gayaneh001@gmail.com>
2019-05-02 21:47:14 +0400

    Initial commit

---
+++ Intro/Intro/AdjacentElementsProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace Intro
+{
+    class AdjacentElementsProduct
+    {
+        public int adjacentElementsProduct(int[] inputArray)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if (inputArray[i] * inputArray[i + 1] > max)
+                {
+                    max = inputArray[i] * inputArray[i + 1];
+                }
+            }
+            return max;
+        }
+    }
+}
