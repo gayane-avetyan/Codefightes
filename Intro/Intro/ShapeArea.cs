9ea3cfe77992739cd91788a13422554ea12502a8
Gayane <gayaneh001@gmail.com>
2019-05-02 22:33:34 +0400

    Initial commit

---
+++ Intro/Intro/ShapeArea.cs
@@ -0,0 +1,15 @@
+﻿namespace Intro
+{
+    class ShapeArea
+    {
+        int shapeArea(int n)
+        {
+            int result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                result += 4 * i;
+            }
+            return result;
+        }
+    }
+}
