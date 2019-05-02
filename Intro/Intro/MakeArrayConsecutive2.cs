f64ea7c3296b8f89cbdd198d741a73d4a2d1f6ed
Gayane <gayaneh001@gmail.com>
2019-05-02 23:01:06 +0400

    Initial commit

---
+++ Intro/Intro/MakeArrayConsecutive2.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Intro
+{
+    class MakeArrayConsecutive2
+    {
+        public int makeArrayConsecutive2(int[] statues)
+        {
+            return statues.Max() - statues.Min() - statues.Length + 1;
+        }
+    }
+}
--- Intro/Intro/ShapeArea.cs
+++ Intro/Intro/ShapeArea.cs
@@ -2,7 +2,7 @@
 {
     class ShapeArea
     {
-        int shapeArea(int n)
+        public int shapeArea(int n)
         {
             int result = 1;
             for (int i = 0; i < n; i++)
