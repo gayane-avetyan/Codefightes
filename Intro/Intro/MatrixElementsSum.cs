47437ee60861eb518916cae05e1e659dc68c2bba
Gayane <gayaneh001@gmail.com>
2019-05-05 00:15:20 +0400

    Initial commit

---
+++ Intro/Intro/MatrixElementsSum.cs
@@ -0,0 +1,22 @@
+﻿namespace Intro
+{
+    class MatrixElementsSum
+    {
+        int matrixElementsSum(int[][] matrix)
+        {
+            int result = 0;
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    if (matrix[j][i] == 0)
+                    {
+                        break;
+                    }
+                    result += matrix[j][i];
+                }
+            }
+            return result;
+        }
+    }
+}
