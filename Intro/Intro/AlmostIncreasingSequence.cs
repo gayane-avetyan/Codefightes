8229d60a0af38c19f4fcb2895c04f61c2454e5b9
Gayane <gayaneh001@gmail.com>
2019-05-04 23:55:47 +0400

    Initial commit

---
+++ Intro/Intro/AlmostIncreasingSequence.cs
@@ -0,0 +1,27 @@
+﻿namespace Intro
+{
+    class AlmostIncreasingSequence
+    {
+        bool almostIncreasingSequence(int[] sequence)
+        {
+            int count = 0;
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                if (sequence[i] >= sequence[i + 1])
+                {
+                    if (sequence.Length > i + 2 && sequence[i] >= sequence[i + 2] && i > 0 && sequence[i - 1] >= sequence[i + 1])
+                    {
+                        return false;
+                    }
+                    else
+                        count++;
+                }
+                if (count > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
