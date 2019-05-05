1a817763aad344c775704ba2cb5e03d38e9039ca
Gayane <gayaneh001@gmail.com>
2019-05-05 18:02:03 +0400

    Initial commit

---
+++ Intro/Intro/SortByHeight.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Intro
+{
+    class SortByHeight
+    {
+        int[] sortByHeight(int[] a)
+        {
+            var people = a.Where(p => p > 0).OrderBy(p => p).ToArray();
+            int index = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > 0)
+                {
+                    a[i] = people[index++];
+                }
+            }
+            return a;
+        }
+    }
+}
