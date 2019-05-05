81cb8e0bbe7182aec2577a380b4d12bc266d109d
Gayane <gayaneh001@gmail.com>
2019-05-05 18:12:02 +0400

    Initial commit

--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -10,6 +10,7 @@ namespace Intro
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ Intro/Intro/ReverseInParentheses.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Intro
+{
+    class ReverseInParentheses
+    {
+        string reverseInParentheses(string inputString)
+        {
+            while (inputString.IndexOf('(') != -1)
+            {
+                int startIndex = inputString.LastIndexOf('(');
+                int endIndex = inputString.IndexOf(')', startIndex) - startIndex + 1;
+                string reverse = new string(inputString.Substring(startIndex, endIndex).Trim('(', ')').Reverse().ToArray());
+                inputString = inputString.Replace(inputString.Substring(startIndex, endIndex), reverse);
+            }
+            return inputString;
+        }
+
+    }
+}
