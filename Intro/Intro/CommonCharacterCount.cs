105f227421f8faf14d891785c1296e10a16cc909
Gayane <gayaneh001@gmail.com>
2019-05-05 17:10:28 +0400

    Initial commit

---
+++ Intro/Intro/CommonCharacterCount.cs
@@ -0,0 +1,23 @@
+﻿namespace Intro
+{
+    class CommonCharacterCount
+    {
+        int commonCharacterCount(string s1, string s2)
+        {
+            int result = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    if (s1[i] == s2[j])
+                    {
+                        result++;
+                        s2 = s2.Remove(j, 1);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
