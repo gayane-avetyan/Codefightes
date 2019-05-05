c95c8b90c5c46eeb8a42d4a86040abb7b0daf05b
Gayane <gayaneh001@gmail.com>
2019-05-05 17:47:15 +0400

    Initial commit

---
+++ Intro/Intro/IsLucky.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Intro
+{
+    class IsLucky
+    {
+        bool isLucky(int n)
+        {
+            int half = n.ToString().Length / 2;
+            return n.ToString().Substring(0, half).Sum(l => Convert.ToInt32(l)) == n.ToString().Substring(half).Sum(r => Convert.ToInt32(r));
+        }
+    }
+}
--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -10,8 +10,23 @@ namespace Intro
     {
         static void Main(string[] args)
         {
-           
+            int n = 1230;
+            int half = n.ToString().Length / 2;
+            string leftPart = n.ToString().Substring(0, half);
+            string rightPart = n.ToString().Substring(half);
+            var result = n.ToString().Substring(0, half).Sum(l => Convert.ToInt32(l));
+            var result2 = n.ToString().Substring(half).Sum(r => Convert.ToInt32(r));
+            Console.WriteLine(result2);
+            //int left = 0;
+            //int right = 0;
+            //for (int i = 0; i < half; i++)
+            //{
+            //    left += leftPart % 10;
+            //    leftPart /= 10;
 
+            //    right += rightPart % 10;
+            //    rightPart /= 10;
+            //}
         }
     }
 }
