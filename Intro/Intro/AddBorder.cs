5439322cb639110b47412fd6978dce4602582dad
Gayane <gayaneh001@gmail.com>
2019-05-06 23:21:27 +0400

    Initial commit

--- Intro/Intro/AddBorder.cs
+++ Intro/Intro/AddBorder.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Intro
+﻿namespace Intro
 {
-    class Program
+    class AddBorder
     {
-        static void Main(string[] args)
+        string[] addBorder(string[] picture)
         {
-            string[] picture = { "abc", "ded" };
             string asterix = new string('*', picture[0].Length + 2);
             string[] result = new string[picture.Length + 2];
             result[0] = asterix;
@@ -19,10 +12,7 @@ namespace Intro
                 result[i] = "*" + picture[i - 1] + "*";
             }
             result[picture.Length + 1] = asterix;
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
+            return result;
         }
     }
 }
