a88826b491b4d23f5f6faf4182dec8ecb76ad7b0
Gayane <gayaneh001@gmail.com>
2019-05-07 20:40:27 +0400

    Initial commit

---
+++ Intro/Intro/PalindromeRearranging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Intro
+{
+    class PalindromeRearranging
+    {
+        bool palindromeRearranging(string inputString)
+        {
+            char[] sortedArray = inputString.ToCharArray();
+            Array.Sort(sortedArray);
+            var even = sortedArray.Where((i, index) => index % 2 == 0).ToArray();
+            var odd = sortedArray.Where((i, index) => index % 2 != 0).ToArray();
+            if (inputString.Length % 2 == 0)
+            {
+                return new string(odd) == new string(even);
+            }
+            if (new string(even.Except(odd).ToArray()).Length <= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
