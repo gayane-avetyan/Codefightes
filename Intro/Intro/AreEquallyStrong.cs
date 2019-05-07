9c8decc17340486566f6c3c8551bdcac1839c87d
Gayane <gayaneh001@gmail.com>
2019-05-07 20:50:24 +0400

    Initial commit

---
+++ Intro/Intro/AreEquallyStrong.cs
@@ -0,0 +1,10 @@
+﻿namespace Intro
+{
+    class AreEquallyStrong
+    {
+        bool areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight)
+        {
+            return yourLeft == friendsLeft && yourRight == friendsRight || yourLeft == friendsRight && yourRight == friendsLeft;
+        }
+    }
+}
