ef8fb899036390ecd9ef242c417803ed638e56be
Gayane <gayaneh001@gmail.com>
2019-05-08 18:40:54 +0400

    Initial commit

---
+++ Intro/Intro/IsIPv4Address.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Intro
+{
+    class IsIPv4Address
+    {
+        bool isIPv4Address(string inputString)
+        {
+            return inputString.Where(i => i == '.').Count() != 3 ? false : !inputString.Split('.').Any(i => i == "" || !int.TryParse(i, out int result) || result < 0 || result > 255);
+        }
+    }
+}
--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -10,6 +10,17 @@ namespace Intro
     {
         static void Main(string[] args)
         {
+            string inputString = "172.16.254.1";
+            var dotCount = inputString.Where(i => i == '.').Count();
+            if (dotCount != 3)
+            {
+                Console.WriteLine(false);
+                return;
+            }
+            var nums = inputString.Split('.');
+            Console.WriteLine(nums.Any(i => int.TryParse(i, out int result) || result < 0 || result > 255));
+
+
         }
     }
 }
