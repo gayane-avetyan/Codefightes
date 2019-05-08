5c726be23819f2966dae7eaf523eb695abecb7aa
Gayane <gayaneh001@gmail.com>
2019-05-08 16:35:44 +0400

    Initial commit

---
+++ Intro/Intro/ArrayMaximalAdjacentDifference.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Intro
+{
+    class ArrayMaximalAdjacentDifference
+    {
+        int arrayMaximalAdjacentDifference(int[] inputArray)
+        {
+            List<int> sum = new List<int>();
+            for (int i = 1; i < inputArray.Length - 1; i++)
+            {
+                sum.Add(Math.Max(Math.Abs(inputArray[i] - inputArray[i - 1]), Math.Abs(inputArray[i] - inputArray[i + 1])));
+            }
+            return sum.Max();
+        }
+    }
+}
--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -10,40 +10,6 @@ namespace Intro
     {
         static void Main(string[] args)
         {
-            //string inputString = "zyryzzzzz";
-            string inputString = "yayaywl";
-            char[] sortedArray = inputString.ToCharArray();
-            Array.Sort(sortedArray);
-            var even = sortedArray.Where((i, index) => index % 2 == 0).ToArray();
-            var odd = sortedArray.Where((i, index) => index % 2 != 0).ToArray();
-           
-            string result = new string(odd) + new string(even);
-            Console.WriteLine(new string(even.Except(odd).ToArray()));
-
-            //Console.WriteLine(new string(result.Reverse().ToArray()));
-
-            //if (inputString.Length % 2 == 0)
-            //{
-            //    Console.WriteLine(new string(odd) == new string(even));
-            //    return;
-            //}
-           
-
-            //if (new string(even.Except(odd).ToArray()).Length == 1)
-            //{
-            //    Console.WriteLine(true);
-            //    return;
-            //}
-
-
-
-
-
-
-
-
-
-
         }
     }
 }
